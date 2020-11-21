cbb2e527bc5b00706473eb208d5ea7f8ff1534ef
baranmmm <m.barann84@gmail.com>
2020-11-21 19:42:31 +0000

    MVC Thymeleaf practices

--- 08-SpringBoot-MVC-Thymeleaf/src/main/java/com/cybertek/controller/StudentController.java
+++ 08-SpringBoot-MVC-Thymeleaf/src/main/java/com/cybertek/controller/StudentController.java
@@ -7,6 +7,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/student")
 public class StudentController {
@@ -20,13 +23,32 @@ public class StudentController {
         model.addAttribute("gender", "male");
 
 
+
+
         //we can pass object through controller
         Student student=new Student(1,"Mehmet", "Baran");
         model.addAttribute("student", student);
 
+        Student student1 = new Student(2,"Sedat Emre", "Baran");
+        Student student2 = new Student(3, "Selcen Azra", "Baran");
+
+        List<Student> studentList =new ArrayList<>();
+        studentList.add(student);
+        studentList.add(student1);
+        studentList.add(student2);
+
+        model.addAttribute("studentList", studentList);
+
 
         return "student/first";
 
+
+    }
+
+    @GetMapping("/goregister")
+    public String home(){
+
+        return "student/register";
 
     }
 }
