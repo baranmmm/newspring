3aac7f6fe3441d4ee6af4a638840a7b21ab849d4
baranmmm <m.barann84@gmail.com>
2020-12-14 02:13:30 +0000

    ORM update

---
+++ Spring-09-ORM-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student(id, first_name, last_name, email) values(1,'Mike','Smith','mike@cybertekschool.com');
+insert into student(id, first_name, last_name, email) values(2,'Adam','Shaun','adam@cybertekschool.com');
+insert into student(id, first_name, last_name, email) values(3,'Tom','Hanks','tom@cybertekschool.com');
+insert into student(id, first_name, last_name, email) values(4,'Brad','Kelly','brad@cybertekschool.com');
