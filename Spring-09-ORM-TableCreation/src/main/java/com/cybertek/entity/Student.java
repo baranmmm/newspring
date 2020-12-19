69f369184b0ab6d2ec200bbc0965e3861bbe949a
baranmmm <m.barann84@gmail.com>
2020-12-19 02:34:01 +0000

    ORM Basics

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -1,17 +1,49 @@
 package com.cybertek.entity;
 
+import com.cybertek.enums.Gender;
+
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
-@Table(name = "students")
+@Table(name = "students")    //This line changes the name of the table
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "studentFirstName")
     private String firstName;
     private String lastName;
     private String email;
+
+    @Transient
+    private String city;
+
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    @Temporal(TemporalType.TIME)
+    private Date birthTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date birthDateTime;
+
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate localDate;
+
+    @Column(columnDefinition = "TIME")
+    private LocalDate localTime;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDate localDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
 
 
 
