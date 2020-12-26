15be02a90deb9425e63140c90b06d797d81a6641
baranmmm <m.barann84@gmail.com>
2020-12-26 04:24:25 +0000

    ORM CinemaApp

---
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Account.java
@@ -0,0 +1,16 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Account extends BaseEntity{
+
+
+}
