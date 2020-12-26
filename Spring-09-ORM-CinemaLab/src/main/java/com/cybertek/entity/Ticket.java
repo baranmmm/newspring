13d355e169ef210e357e4d75bf36b43ecac16c65
baranmmm <m.barann84@gmail.com>
2020-12-26 04:23:36 +0000

    ORM CinemaApp

---
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Ticket.java
@@ -0,0 +1,34 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Ticket extends BaseEntity{
+
+    private Integer seatNumber;
+    private Integer rowNumber;
+
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime localDateTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private MovieCinema movieCinema;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_account_id")
+    private User user;
+
+    public Ticket(Integer seatNumber, Integer rowNumber, LocalDateTime localDateTime) {
+        this.seatNumber = seatNumber;
+        this.rowNumber = rowNumber;
+        this.localDateTime = localDateTime;
+    }
+}
